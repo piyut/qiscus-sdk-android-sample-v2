0cfb88bd3a18d9d4ea5e2f678e6d46c97609249c
omayib <omayib@gmail.com>
2017-09-21 20:32:47 +0700

    requesting the alumnus data from server

--- app/src/main/java/id/technomotion/AlumniListActivity.java
+++ app/src/main/java/id/technomotion/AlumniListActivity.java
@@ -6,14 +6,25 @@ import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.UUID;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * Created by omayib on 18/09/17.
  */
 
 public class AlumniListActivity extends Activity{
+    private static final String TAG = "AlumniListActivity";
     private RecyclerView mRecyclerView;
     private LinearLayoutManager mLinearLayoutManager;
     private RecyclerAdapter mAdapter;
@@ -25,15 +36,39 @@ public class AlumniListActivity extends Activity{
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerViewAlumni);
         mLinearLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLinearLayoutManager);
-        ArrayList<Alumni> alumnis = new ArrayList<>();
-        alumnis.add(new Alumni("21dd","Andri Atmojo","andri@yahoo.com", "Freelance Android"));
-        alumnis.add(new Alumni("21ds","Joki Widyawan","joki@yahoo.com", "Freelance Web"));
-        alumnis.add(new Alumni("2eds","Eko Purnawan","eko@yahoo.com", "Freelance Web"));
-        alumnis.add(new Alumni("2gds","Susilo Andi S","silo@yahoo.com", "Freelance iOS"));
-        alumnis.add(new Alumni("3gds","Prakoso Binar","binar@yahoo.com", "Freelance Android"));
-        alumnis.add(new Alumni("53ds","Gilang Santoso","gilang@yahoo.com", "Freelance Web"));
-        alumnis.add(new Alumni("3tgs","Atmojo Rakoso","mojo@yahoo.com", "Freelance Backend"));
+        final ArrayList<Alumni> alumnis = new ArrayList<>();
+
         mAdapter = new RecyclerAdapter(alumnis);
         mRecyclerView.setAdapter(mAdapter);
+
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url("http://10.0.2.2:3000/alumnus").build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    JSONArray data = new JSONArray(response.body().string());
+                    for (int i = 0; i < data.length(); i++) {
+                        JSONObject obj = data.getJSONObject(i);
+                        alumnis.add(new Alumni(obj.getString("id"),obj.getString("name"),obj.getString("email"),obj.getString("work")));
+                    }
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mAdapter.notifyDataSetChanged();
+                        }
+                    });
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
